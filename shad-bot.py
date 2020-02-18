6210c6d83e62a8443a8b4e9b97b5338771320bcd
Jameson Lee <jameson.developer@gmail.com>
2020-02-17 20:43:16 -0800

    first two bots

---
+++ shad-bot.py
@@ -0,0 +1,29 @@
+# Work with Python 3.6
+import discord
+import json
+TOKEN = json.loads(open(".secrets", "r").read())["shad"]
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('Bug'):
+        msg = 'WHERE MAMA WHERE?!?!?!!? \nhttps://giphy.com/gifs/superman-phone-looking-c20UV66B7zCWA'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
